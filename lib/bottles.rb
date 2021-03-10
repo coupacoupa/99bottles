8a9655a479174407bc6936f5aba14c9949f44e67
coupacoupa <44894563+coupacoupa@users.noreply.github.com>
2021-03-10 19:44:51 +0800

    First iteration

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,43 @@
 class Bottles
+    def song()
+        verses(99, 0)
+    end
+
+    def verses(n, m)
+        d = ""
+        loop do
+            d << verse(n)
+
+            if (n != m)
+                d << "\n"
+            end
+            n = n - 1
+        break if  (n < m)
+        end
+        return d
+    end
+
     def verse(n)
         return <<-VERSE
-#{getText("No more", n, 0, n)} #{getText("bottle", n, 1, "bottles")} of beer on the wall, #{getText("no more", n, 0, n)} #{getText("bottle", n, 1, "bottles")} of beer.
-Take #{getText("it", n, 1, "one")} down and pass it around, #{getText("no more", n - 1, 0, n - 1)} #{getText("bottle", n - 1, 1, "bottles")} of beer on the wall.
+#{getNumberedText("bottle", n, 1).capitalize} of beer on the wall, #{getNumberedText("bottle", n, 1)} of beer.
+#{getVerseLine(n)}, #{getNumberedText("bottle", n - 1, 1)} of beer on the wall.
 VERSE
     end
 
-    def getText(base_text, n, m, condition_text)
+    def getNumberedText(base_text, n, m)
         # if {n} == {m}, {base_text} else {conditional_text}
-        result = n == m ? base_text : condition_text
+        result = "#{n == 0 ? "no more": n % 100} #{n == m ? base_text : base_text + "s"}"
     end
+
+    def getVerseLine(n)
+        return n == 0 ? "Go to the store and buy some more" : "Take #{n == 1 ? "it" : "one"} down and pass it around"
+    end
+
+    #template
+    # {n} text, [{n} / No more] [bottle / bottles] [of beer on the wall[,]]
+    # {n} text. [{n} / no more] [bottle / bottles] [of beer[.]]
+    # text,     [Take [one/it] down and pass it around, / Go to the store  and buy some more[,]] 
+    # {n} text. [{n} / no more] [bottle / bottles] [of beer on the wall[.]]
+
+    # meant for creating [{n} / No more] [bottle / bottles]
 end
